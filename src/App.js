b552d596588a01b3688534c2282bc6dd60671fb1
gunjan <gunjant@webgility.com>
2020-02-03 16:03:22 +0530

    section 1 and section 2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Fragment, Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  render() {
+    const name = 'abc';
+    const loading = false;
+    const showName = false;
+
+
+      return (
+        <Fragment>
+          {loading ? <h1>hello {name}</h1>:<h4>hello {showName && '111'}</h4>}
+        </Fragment>
+      );
+
+  }
 }
 
 export default App;
