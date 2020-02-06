904de975e3beb0b0119fe7aa14dec2d6995551d9
gunjan <gunjant@webgility.com>
2020-02-06 11:40:24 +0530

    spinner comp

--- src/App.js
+++ src/App.js
@@ -3,8 +3,20 @@ import Navbar from './components/layout/Navbar'
 import UserItem from './components/Users/UserItem'
 import './App.css';
 import Users from './components/Users/Users';
+import axios from 'axios';
 
 class App extends Component{
+  state = {
+    users: [],
+    loading: false
+  }
+  async componentDidMount() {
+    this.setState({loading:true});    
+    console.log('cmp mount called');
+    const res = await axios.get('https://api.github.com/users')
+    console.log(res.data);
+    this.setState({users:res.data, loading:false})
+  }
   render() {
     const name = 'abc';
     const loading = true;
@@ -14,7 +26,7 @@ class App extends Component{
       return (
         <div>
           <Navbar/>
-          <Users/>
+          <Users users={this.state.users} loading={this.state.loading}/>
         </div>
       );
 
--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,41 +1,20 @@
 import React, { Component } from 'react'
 import UserItem from './UserItem'
 import PropTypes from 'prop-types'
+import Spinner from '../layout/Spinner'
 
-
-class Users extends Component {
-    state = {
-        users : [
-            {
-                "login": "mojombo",
-                "id": 1,
-                "avatar_url": "https://avatars0.githubusercontent.com/u/1?v=4",
-                "html_url": "https://github.com/mojombo"
-            },
-            {
-                "login": "defunkt",
-                "id": 2,
-                "avatar_url": "https://avatars0.githubusercontent.com/u/2?v=4",
-                "html_url": "https://github.com/defunkt"
-            },
-            {
-                "login": "pjhyett",
-                "id": 3,
-                "avatar_url": "https://avatars0.githubusercontent.com/u/3?v=4",
-                "html_url": "https://github.com/pjhyett"
-            }            
-        ]
-    }
-
-    render() {
+const Users = ({users, loading}) => {
+    if(loading) {
+        return (<Spinner/>)            
+    } else {
         return (
             <div>
-               {this.state.users.map(user=>
+            {users.map(user=>
                 <UserItem key={user.id} user={user}/>
-               )}                
+            )}                
             </div>
         )
     }
 }
-
+        
 export default Users
