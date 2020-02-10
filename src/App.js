7f13fa0ebd68595f9a26cf1d16d304be08321376
gunjan <gunjant@webgility.com>
2020-02-10 12:49:27 +0530

    search comp

--- src/App.js
+++ src/App.js
@@ -4,18 +4,27 @@ import UserItem from './components/Users/UserItem'
 import './App.css';
 import Users from './components/Users/Users';
 import axios from 'axios';
+import Search from './components/Users/Search'
 
 class App extends Component{
   state = {
     users: [],
     loading: false
   }
-  async componentDidMount() {
-    this.setState({loading:true});    
-    console.log('cmp mount called');
-    const res = await axios.get('https://api.github.com/users')
-    console.log(res.data);
-    this.setState({users:res.data, loading:false})
+  // async componentDidMount() {
+  //   this.setState({loading:true});    
+  //   console.log('cmp mount called');
+  //   const res = await axios.get('https://api.github.com/users')
+  //   console.log(res.data);
+  //   this.setState({users:res.data, loading:false})
+  // }
+  searchUsers = async (text) => {
+    this.setState({loading:true});
+    console.log('parent called');
+    console.log(text);
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}`)
+    console.log(res.data.items);
+    this.setState({users:res.data.items, loading:false})    
   }
   render() {
     const name = 'abc';
@@ -26,6 +35,7 @@ class App extends Component{
       return (
         <div>
           <Navbar/>
+          <Search searchUsers={this.searchUsers}/>
           <Users users={this.state.users} loading={this.state.loading}/>
         </div>
       );
