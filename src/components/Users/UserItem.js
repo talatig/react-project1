421f7fda3f93b87a6aba661c4b4e6e73513d0fac
gunjan <gunjant@webgility.com>
2020-07-05 16:18:53 +0530

    class based Component to functional Component

--- src/components/Users/UserItem.js
+++ src/components/Users/UserItem.js
@@ -1,25 +1,38 @@
 import React, { Component } from 'react'
 
-class UserItem extends Component {
+// class UserItem extends Component {
 
-    state = {
-        id: '1',
-        login: 'mojombo',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-        html_url: 'https://github.com/mojombo'
-    }
+//     state = {
+//         id: '1',
+//         login: 'mojombo',
+//         avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
+//         html_url: 'https://github.com/mojombo'
+//     }
 
-    render() {
-        const {id,login,avatar_url,html_url} = this.props.user;
-        return (
-            <div>
-                <p>{id}</p>
-                <p>{login}</p>
-                <img style={{width: '60px'}} className='round-img' src={avatar_url}/>
-                <p>{html_url}</p> 
-            </div>
-        )
-    }
+//     render() {
+//         const {id,login,avatar_url,html_url} = this.props.user;
+//         return (
+//             <div>
+//                 <p>{id}</p>
+//                 <p>{login}</p>
+//                 <img style={{width: '60px'}} className='round-img' src={avatar_url}/>
+//                 <p>{html_url}</p> 
+//             </div>
+//         )
+//     }
+// }
+
+//converting above class based Component to functional Component
+const UserItem =(props) => {
+    const {id,login,avatar_url,html_url} = props.user;
+    return (
+        <div>
+            <p>{id}</p>
+            <p>{login}</p>
+            <img style={{width: '60px'}} className='round-img' src={avatar_url}/>
+            <p>{html_url}</p> 
+        </div>
+    )
 }
 
 export default UserItem
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,26 +1,49 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
-export class Navbar extends Component {
-    static defaultProps = {
-        title : 'Github Finder',
-        icon : 'fab fa-github'
-    }
+// export class Navbar extends Component {
+//     static defaultProps = {
+//         title : 'Github Finder',
+//         icon : 'fab fa-github'
+//     }
 
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-        icon: PropTypes.string.isRequired
-    }
-    render() {
-        return (
-            <nav className="navbar bg-primary">
-                <h1>
-                    <i className={this.props.icon}></i>
-                     {this.props.title}
-                </h1>
-            </nav>
-        )
-    }
+//     static propTypes = {
+//         title: PropTypes.string.isRequired,
+//         icon: PropTypes.string.isRequired
+//     }
+//     render() {
+//         return (
+//             <nav className="navbar bg-primary">
+//                 <h1>
+//                     <i className={this.props.icon}></i>
+//                      {this.props.title}
+//                 </h1>
+//             </nav>
+//         )
+//     }
+// }
+
+//converting above class based Component to functional Component
+const Navbar = (props) => {
+    const {icon,title} = props;
+    return (
+        <nav className="navbar bg-primary">
+            <h1>
+                <i className={icon}></i>
+                    {title}
+            </h1>
+        </nav>        
+    )
+}
+
+Navbar.defaultProps = {
+    title: 'Github Finder',
+    icon: 'fab fa-github'
+}
+
+Navbar.propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired
 }
 
 export default Navbar
