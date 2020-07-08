552c1ed50877134499f5389f75fdf83868ec65cb
gunjan <gunjant@webgility.com>
2020-07-08 15:38:47 +0530

    back to search

--- src/components/Users/User.js
+++ src/components/Users/User.js
@@ -1,6 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom';
+
 
 export class User extends Component {
+
+    staticpropTypes = {
+        loading: PropTypes.bool,
+        user: PropTypes.object.isRequired,
+        getUser: PropTypes.func.isRequired,
+    }
     componentDidMount(){
         console.log(this.props.match.params.login);
         this.props.getUser(this.props.match.params.login);
@@ -9,10 +19,13 @@ export class User extends Component {
         const {name,avatar_url,location,bio,blog,login,html_url,followers,following} = this.props.user;
 
         const {loading} = this.props;
+
+        if(loading)
+        return <Spinner/>
         return (
-            <div>
-                {name}
-            </div>
+            <Fragment>
+                <Link to = "/" className="btn btn-light">Back to search</Link>                
+            </Fragment>
         )
     }
 }
